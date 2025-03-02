249c1c0836368db326808b0c9b14457823927931
Nicolas Castellanos <nicolasfelipe00234@gmail.com>
2025-03-02 17:30:35 -0500

    fetched api and displayed

--- apps/spacex/views.py
+++ apps/spacex/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+import requests
+
+URL = "https://api.spacexdata.com/v3/launches"
 
 
 def spacex(request):
-    return render(request, "pages/spacex.html")
+    response = requests.get(URL)
+    data = response.json()
+    return render(request, "pages/spacex.html", {"launches": data})
 
 
 # Create your views here.
