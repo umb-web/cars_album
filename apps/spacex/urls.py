a7500cb2fe387c887574d5cae9145e4e169c0aba
Nicolas Castellanos <nicolasfelipe00234@gmail.com>
2025-03-02 20:20:41 -0500

    ui to display laucn info

--- apps/spacex/urls.py
+++ apps/spacex/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.spacex, name="spacex"),
+    path("<int:pk>/", views.launch_info, name="launch_info"),
 ]
--- apps/spacex/views.py
+++ apps/spacex/views.py
@@ -10,4 +10,11 @@ def spacex(request):
     return render(request, "pages/spacex.html", {"launches": data})
 
 
+def launch_info(request, pk):
+    LAUNCH_URL = f"https://api.spacexdata.com/v3/launches/{pk}/"
+    response = requests.get(LAUNCH_URL)
+    data = response.json()
+    return render(request, "pages/launch.html", {"launch": data})
+
+
 # Create your views here.
